00c1b2cc7a8466d2f3fd4eea27142b510ffb9f5f
wangxiangna <2603121563@qq.com>
2021-01-18 09:07:26 +0800

    引入接口

--- src/api/api.js
+++ src/api/api.js
@@ -2,42 +2,62 @@ import fetch from './http';
 export default {
   /**
    * 学生登录
-   * @param  {[type]} params [description]
-   * @return {[type]}        [description]
    */
   studentlogin(params) {
     return fetch.fetchGet('student/login', params);
   },
   /**
    * 管理员登录
-   * @param  {[type]} params [description]
-   * @return {[type]}        [description]
    */
   managelogin(params) {
     return fetch.fetchGet('manage/login', params);
   },
   /**
    * 获取学生列表
-   * @param  {[type]} params [description]
-   * @return {[type]}        [description]
    */
   studentlist(params) {
     return fetch.fetchGet('student/list', params);
   },
   /**
    * 添加编辑学生
-   * @param  {[type]} params [description]
-   * @return {[type]}        [description]
    */
   addstudent(params) {
     return fetch.fetchGet('add/student', params);
   },
   /**
    * 删除学生
-   * @param  {[type]} params [description]
-   * @return {[type]}        [description]
    */
   delstudent(params) {
     return fetch.fetchGet('del/student', params);
+  },
+  /**
+   * 修改学生密码
+   */
+  updatestudentpwd(params) {
+    return fetch.fetchGet('update/student/pwd', params);
+  },
+  /**
+   * 获取课程列表
+   */
+  courselist(params) {
+    return fetch.fetchGet('course/list', params);
+  },
+  /**
+   * 添加课程
+   */
+  addcourse(params) {
+    return fetch.fetchGet('add/course', params);
+  },
+  /**
+   * 删除课程
+   */
+  delcourse(params) {
+    return fetch.fetchGet('delete/course', params);
+  },
+  /**
+   * 添加管理员
+   */
+  addadmin(params) {
+    return fetch.fetchGet('add/admin', params);
   },
 }
